a6220c62908c3c25d83d3c4b1eaccbad0588fa9c
Meta1core <howk20000@gmail.com>
2020-05-04 12:43:54 +0300

    changes to heroku

--- src/app/drugs.service.ts
+++ src/app/drugs.service.ts
@@ -18,20 +18,20 @@ export class DrugsService {
   //   return this.http.get("http://api.tab.by/regions/list", { headers: headers })
   //   } 
   getRegions(): Observable<any> {
-    return this.http.get("http://localhost:8080/drugs/regions");
+    return this.http.get("https://api-drugsearcher.herokuapp.com/drugs/regions");
   } 
   getDrugs(drugName : string, region: number): Observable<any> {
     if(region == 0) {
     let headers = new HttpHeaders();
     headers.append('Content-Type', 'application/json');
     let params = new HttpParams().set("drugName",drugName);
-    return this.http.get("http://localhost:8080/drugs/search", {params: params});
+    return this.http.get("https://api-drugsearcher.herokuapp.com/drugs/search", {params: params});
     }
     else {
       let headers = new HttpHeaders();
       headers.append('Content-Type', 'application/json');
       let params = new HttpParams().set("drugName",drugName).set("region", region.toString());
-      return this.http.get("http://localhost:8080/drugs/search", {params: params});
+      return this.http.get("https://api-drugsearcher.herokuapp.com/drugs/search", {params: params});
     }
   } 
   findPharmacy(ls_num: string, region: number): Observable<any> {
@@ -39,20 +39,20 @@ export class DrugsService {
     let headers = new HttpHeaders();
     headers.append('Content-Type', 'application/json');
     let params = new HttpParams().set("ls_num",ls_num);
-    return this.http.get("http://localhost:8080/drugs/pharmacy", {params: params});
+    return this.http.get("https://api-drugsearcher.herokuapp.com/drugs/pharmacy", {params: params});
     }
     else {
       let headers = new HttpHeaders();
       headers.append('Content-Type', 'application/json');
       let params = new HttpParams().set("ls_num",ls_num).set("region", region.toString());
-      return this.http.get("http://localhost:8080/drugs/pharmacy", {params: params});
+      return this.http.get("https://api-drugsearcher.herokuapp.com/drugs/pharmacy", {params: params});
     }
   } 
   aboutPharmacy(name:string, ls_num:number, region:number): Observable<any> {
     let headers = new HttpHeaders();
     headers.append('Content-Type', 'application/json');
     let params = new HttpParams().set("name",name).set("ls_num", ls_num.toString()).set("region",region.toString());
-    return this.http.get("http://localhost:8080/drugs/pharmacy/about", {params: params});
+    return this.http.get("https://api-drugsearcher.herokuapp.com/drugs/pharmacy/about", {params: params});
   }
   checkPython(): Observable<any>{
     return this.http.get("http://localhost:5000/")
